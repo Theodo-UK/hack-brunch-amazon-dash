0854746ebf34b308069b908ad64f22b0a2920639
Ben Ellerby <benellerby@hotmail.co.uk>
2018-02-13 19:08:13 +0000

    custom speaking

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+const say = require('say')
+
+app.use(bodyParser())
+app.use(express.static('public'))
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
   res.send('Hello Theodo!');
 });
+
+app.post('/say/', function (req, res) {
+  const body = req.body;
+  const text = body.name_field;
+  const voice = body.voice_name;
+  say.speak(text, voice);
+});
+
+
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
